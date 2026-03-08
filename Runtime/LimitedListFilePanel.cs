2afc6893c93fbe6eceb47e8cce54bbb088f4fd0f
Glurth <Glurth@gmail.com>
2026-03-08 11:01:08 -0600

    changed checkfor skipping update of details text on callback.

--- Runtime/LimitedListFilePanel.cs
+++ Runtime/LimitedListFilePanel.cs
@@ -748,9 +748,14 @@ namespace EyE.Unity.UI
                 customGetFileDetailsDisplayCallbackStringFunction(fileInfo, (updatedResult) =>
                 {
                     // Only overwrite if the UI is still relevant to this file
-                    if (hoverAndSelectedFileDetails != null && (selectedFile == fileInfo || displayedFiles.Contains(fileInfo)))
+                    if (hoverAndSelectedFileDetails != null && (selectedFile.FullName.Equals(fileInfo.FullName)))// || displayedFiles.Contains(fileInfo)))
                     {
+                       // Debug.Log("updating hoverAndSelectedFileDetails from callback");
                         hoverAndSelectedFileDetails.text = updatedResult;
+                    }
+                    else
+                    {
+                       // Debug.Log("NOT updating hoverAndSelectedFileDetails from callback- ignored.  hoverAndSelectedFileDetails != null:"+(hoverAndSelectedFileDetails != null)+ "  selectedFile.Equals(fileInfo):"+ selectedFile.Equals(fileInfo)+ "displayedFiles.Contains(fileInfo):"+ displayedFiles.Contains(fileInfo));
                     }
                 });
             }
